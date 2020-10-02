165b9ac51b113cb4bd55d68a1c890e578d8d0229
LemonskyChara <71406789+LemonskyChara@users.noreply.github.com>
2020-10-02 12:38:02 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,4 +1,4 @@
-a = int(input("Enter a integer of width < 10: "))
-b = int(input("Enter a integer of height < 10: "))
+a = int(input(""))
+b = int(input(""))
 for i in range(1,b+1):
     print("*" * a)
