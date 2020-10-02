b834f8e51e235b8287b0ab5df1854cb4fad143ad
LemonskyChara <71406789+LemonskyChara@users.noreply.github.com>
2020-10-02 12:33:54 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,10 +1,9 @@
-
 name =""
 
 nameList = ("Lebron","Kobe","Michale","Shaq","Dennis")
 
 for name in nameList:
-    name = str(input("Enter a name: "))
+    name = str(input("Enter a name: ")).strip()
     if name in nameList:
         print("That name is in the list")
     else:
