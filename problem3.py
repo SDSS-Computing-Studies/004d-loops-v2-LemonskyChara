2d58dd4f3e9eeda80e23053cfa47a2fc01f76174
LemonskyChara <71406789+LemonskyChara@users.noreply.github.com>
2020-10-02 12:22:20 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,7 +1,7 @@
-a = int(input("Enter a number "))
+a = int(input("Enter a number: "))
 sum = 0
 for i in range(1,a+1):
     while i > 0:
         sum += 10**(i - 1)
         i -= 1
-print(sum)
+print("the sum of the series is " + str(sum))
